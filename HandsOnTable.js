7ff6e804296ae1919f87c802e5583c882acd1b58
Jenny Astrachan <jennya@mit.edu>
2014-11-23 16:02:40 -0500

    updated to new user

--- HandsOnTable.js
+++ HandsOnTable.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
 	transport = new Thrift.Transport("http://datahub.csail.mit.edu/service/json"),
 	protocol = new Thrift.Protocol(transport),
 	client = new DataHubClient(protocol),
-	con_params = new ConnectionParams({'user': 'jlastrachan', 'password': 'jenny0531'}),
+	con_params = new ConnectionParams({'user': 'finalproject6830', 'password': 'databases'}),
 	con = client.open_connection(con_params),
-	res = client.execute_sql(con, 'select * from jlastrachan.test.cpw_events');
+	res = client.execute_sql(con, 'select * from finalproject6830.test.cpw_events');
 
 	// $('#results').append('<tr><th>' + res.field_names.join('</th><th>') + '</th></tr>');
 
