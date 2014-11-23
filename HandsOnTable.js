e63bfbf6fe965f2a86e5ee5f8f4a6b026ac2b719
Jenny Astrachan <jennya@mit.edu>
2014-11-23 15:57:47 -0500

    Adding sample code

---
+++ HandsOnTable.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	transport = new Thrift.Transport("http://datahub.csail.mit.edu/service/json"),
+	protocol = new Thrift.Protocol(transport),
+	client = new DataHubClient(protocol),
+	con_params = new ConnectionParams({'user': 'jlastrachan', 'password': 'jenny0531'}),
+	con = client.open_connection(con_params),
+	res = client.execute_sql(con, 'select * from jlastrachan.test.cpw_events');
+
+	// $('#results').append('<tr><th>' + res.field_names.join('</th><th>') + '</th></tr>');
+
+	// $.each(res.tuples, function(tuple_idx, tuple) {
+	//   $('#results').append('<tr><td>' + tuple.cells.join('</td><td>') + '</td></tr>');
+	// });
+	var data = res.tuples.map(function (tuple) { return tuple.cells; });
+	$('#results').handsontable({
+		data: data,
+		minSpareRows: 1,
+		colHeaders: true,
+		contextMenu: true
+	});
+});
